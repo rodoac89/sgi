be3f6bf03abfdd9bedf1dac4d0702255669d2410
Rodolfo Aravena Collipal <rodo.aravena@outlook.com>
2021-11-18 21:34:59 -0300

    Load

--- backend/apps/core/views.py
+++ backend/apps/core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from apps.core.models import Room
 
@@ -12,4 +12,11 @@ def dashboard(request):
     context['rooms'] = rooms
     print(rooms)
     return render(request, template_name, context)
+
+def load(request):
+    from django.contrib.auth.models import User
+    user = User.objects.create_user(username='labs',
+                                 email='',
+                                 password='1234')
+    return redirect('dashboard')
 
