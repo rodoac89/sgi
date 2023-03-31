b2e291d36128dd0240e016cf0ba560311a0f6216
fperez <fperez@wholemeaning.com>
2023-03-30 23:08:22 -0300

    Agregar activity app y task

---
+++ src/activity/ActivityApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Timers;
+using System.Windows;
+
+namespace ActivityApp
+{
+    public partial class MainWindow : Window
+    {
+        private static Timer? shutdownTimer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Topmost = true;
+
+            shutdownTimer = new Timer { Interval = 60000 };
+            shutdownTimer.Elapsed += new ElapsedEventHandler(ShutdownTask);
+            shutdownTimer.Start();
+        }
+
+        private void Btn_Shutdown_Click(object sender, RoutedEventArgs e)
+        {
+            ShutdownPC();
+        }
+
+        private void Btn_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ShutdownPC()
+        {
+            // Process.Start("shutdown", "/s /t 0");
+            MessageBox.Show("APAGANDO");
+            Close();
+        }
+
+        private void ShutdownTask(object? source, ElapsedEventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                shutdownTimer?.Stop();
+                ShutdownPC();
+                shutdownTimer?.Start();
+            }));            
+        }
+    }
+}
