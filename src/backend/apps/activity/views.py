db0ea9231739228e5bbc8cf1a5d55015cc637c75
fperez <fperez@wholemeaning.com>
2023-05-10 16:18:27 -0400

    Agregar decorador login required a las views de activity y agregar condicional para que no crashee cuando no hay campuses creados

--- src/backend/apps/activity/views.py
+++ src/backend/apps/activity/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from apps.core.models import Campus, Room, Workstation
-from apps.activity.models import Session
-from .utils import formatTimestamp
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     campuses = Campus.objects.all().values()
-    first_campus_id = campuses.first()["id"]
-    rooms = Room.objects.filter(campus_id=first_campus_id).values()
-    first_room_id = rooms.first()["id"]
-    workstations = workstations = Workstation.objects.filter(room_id=first_room_id).values()
+    if campuses:
+        first_campus_id = campuses.first()["id"]
+        rooms = Room.objects.filter(campus_id=first_campus_id).values()
+        if rooms:
+            first_room_id = rooms.first()["id"]
+            workstations = workstations = Workstation.objects.filter(room_id=first_room_id).values()
+        else:
+            workstations = []
+    else:
+        rooms = []
+        workstations = []
         
     return render(request, "index.html", {
         "campuses": campuses,
@@ -16,10 +23,14 @@ def index(request):
         "workstations": workstations
     })
 
+@login_required
 def state(request):
     campuses = Campus.objects.all().values()
-    first_campus_id = campuses.first()["id"]
-    rooms = Room.objects.filter(campus_id=first_campus_id).values()
+    if campuses:
+        first_campus_id = campuses.first()["id"]
+        rooms = Room.objects.filter(campus_id=first_campus_id).values()
+    else:
+        rooms = []
 
     return render(request, "current_state.html", {
         "campuses": campuses,
