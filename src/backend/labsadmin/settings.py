8a214ad9b88ade3e71be67fab072a87947c6f3ef
fperez <fperez@wholemeaning.com>
2023-05-10 11:30:09 -0400

    Agregar collectstatics 2

--- src/backend/labsadmin/settings.py
+++ src/backend/labsadmin/settings.py
@@ -273,9 +273,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
